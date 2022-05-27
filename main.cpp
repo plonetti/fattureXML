5d4bedddefdf4e6decd8acb3af4eda73e5ef7fbb
Pasquale <lonetti@paciunzio.istruzioneer.it>
2022-05-27 17:37:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
 #include "src/pugixml.hpp"
-
 #include <filesystem>
+
+#include <cstdio>
+#include <ctime>
+
 using std::filesystem::directory_iterator;
+
 using namespace std;
-
-
+// ********  Funzione esempio 1 *************//
 void getRootName(){
     pugi::xml_document doc;
     doc.load_file("Fattura1.xml");
-
     pugi::xml_node node = doc.first_child();
     cout<<"Root Name: "<<node.name()<<endl;
 }
-void getRootName1(){
-    pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file("FATT/Fattura1.xml");
-
-    pugi::xml_node node = doc.first_child();
-    cout<<"Root Name: "<<node.name()<<endl;
-}
+// ********  Funzione esempio 2 *************//
 void getNodeValue(){
     pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file("Fattura1.xml");
+    pugi::xml_parse_result result = doc.load_file("FATT/Fattura1.xml");
     pugi::xml_node node=doc.first_child();
     cout << node.name()<<endl;
     pugi::xml_node nodeChild=node.child("FatturaElettronicaBody").child("DatiPagamento").child("DettaglioPagamento").child("ImportoPagamento");
-    //cout<<"NODE:ImportoPagamento" << nodeChild.name()<< endl;
-   cout<<"VALUE: "<<nodeChild.child_value()<<endl;
+    cout<<"NODE:ImportoPagamento" <<endl;
+    cout<<"VALUE: "<<nodeChild.child_value()<<endl;
 }
 
+// ********  Funzione esempio 3 *************//
 void listDir(){  
     string path = "FATT";
     for (const auto & file : directory_iterator(path))
         cout << file.path().string() << endl;
 }
 
+//**** Funzione calcola importo talale delle fatture ****** //
 void getImportoAllFatt(){
    string path = "FATT";
    // const char* path = R"(FATT)";
@@ -52,25 +50,77 @@ void getImportoAllFatt(){
         
         nodeValue=nodeChild.child_value();
         string fs(nodeValue);
-        //totale += atof(fs.c_str());
-        cout <<nodeChild.child_value()<<endl;
-       
+        totale += atof(fs.c_str());       
     }
-   //  cout<<"IMPORTO TOTALE DELLE FATTURE :" << totale;
-        
+    cout<<"IMPORTO TOTALE DELLE FATTURE :" << totale<< endl;
 }
 
+//****** Funzioni calcola data Corrente ************* //
+const std::string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "%Y-%m-%d-%H%M%S", &tstruct);
 
+    return buf;
+}
 
+// ******** Funzione rinomina file elaborati *******//
+void renameAllFile(){
+    string path = "FATT";
+    string s = currentDateTime();
+    for (const auto & file : directory_iterator(path)){      
+       rename(file.path().string().c_str(), (file.path().string()+s).c_str()); 
+    }
+}
+
+// ******** Funzione sposta file elaborati ******//
+void moveAllFileXML(){
+    system("move \"fatt\\*\" \"fatt1\" ");
+}
+
+// ******** Funzione Menu ******//
+int menu(void){
+  int selezione = 0;
+  do{
+    cout<<endl<<"1 - Trova importo totale"<<endl;
+    cout<<"2 - Sposta le fatture elaborate"<<endl;
+    cout<<"3 - Esci"<<endl;
+    cout<<"Effettua una scelta:"<<endl;
+    cin>>selezione;
+    }
+    while (selezione< 1 || selezione>3);
+  return selezione;
+}
 int main()
 {
-   // getRootName();
-   getImportoAllFatt();
-    //system("move \"FATT\\*" "FATT1"); 
+    int scelta;
+    do{
+        //system ("cls");
+        scelta=menu();
+        switch (scelta){
+            case 1:
+            {
+             getImportoAllFatt();
+             break;
+            }
+            case 2:
+            {
+                renameAllFile();
+                moveAllFileXML();
+                break;
+            }
+            default:
+                {
+                    cout << " scelta non valida \n";
+                }
+            break;
+        }
+    } while(scelta!= 3);
+    
     cout<<" \n*********FINE ELABORAZIONE***********************\n";
     //cin.get();
     //cin.get();
     return 0;
 }
-
-
